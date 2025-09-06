bfe8f3c81200d4a26e4fbb5dadde0cb6e536f129
azmisal <b20cs33@gectcr.ac.in>
2025-09-06 13:08:20 +0530

    ADD: added contact us page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
     { title: "Services", href: "/page-not-found" },
     { title: "Careers", href: "/page-not-found" },
     { title: "Clients", href: "/page-not-found" },
-    { title: "Contact Us", href: "/page-not-found" },
+    { title: "Contact Us", href: "/contact" },
   ];
 
   return (
